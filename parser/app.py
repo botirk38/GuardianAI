d63cef7ba00fb0137d4bb804a8a27b13f2ccfb6b
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2024-03-09 19:44:31 +0000

    integrate fetch endpoint

--- parser/app.py
+++ parser/app.py
@@ -2,9 +2,9 @@ from flask import jsonify
 
 
 from ast_analysis.ast_mock_data import vulnerability_pattern, mock_submission_ast1 
-from ast_analysis.ast_mock_data import mock_submission_ast2 , rawdata
+from ast_analysis.ast_mock_data import mock_submission_ast2
 from ast_analysis.ast_comparer import compare_ast
-from ast_analysis.ast_simplfy import simplify_ast
+from ast_analysis.ast_simplfy import simplify_ast, fetch_ast
 from settings import app
 
 
@@ -19,13 +19,11 @@ def hello_world():
 
 @app.route('/test_analyze', methods=['GET'])
 def test_analyze():
-    #found_vulnerabilities = compare_ast(mock_submission_ast1, vulnerability_pattern)
-        vulnerability_found = compare_ast(mock_submission_ast1, vulnerability_pattern)
-        simplified_ast = simplify_ast(rawdata)
+        raw_data = fetch_ast()
+        simplified_ast = simplify_ast(raw_data)
         response_data = {
-        "vulnerability_found": "yes" if vulnerability_found else "no",
         "simplified_ast": simplified_ast }
-        answer = compare_ast(rawdata , vulnerability_pattern)
+        answer = compare_ast(raw_data , vulnerability_pattern)
         print(answer)
         return jsonify(response_data)
 
